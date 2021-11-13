53a03bb6068ca865d7d99fdfc045f84e5e3130c5
Juan Camilo Sánchez Taveras <jsanchez@universal.com.do>
2021-11-12 21:08:40 -0400

    Adding Categories Manager implementation

---
+++ Core/Managers/Implementations/CategoriesManager.cs
@@ -0,0 +1,39 @@
+﻿using FinanceManagement.Core.Entities;
+using FinanceManagement.Core.Repositories;
+using FinanceManagement.Core.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceManagement.Core.Managers.Implementations
+{
+    public class CategoriesManager
+    {
+        private readonly IUnitOfWork UnitOfWork;
+
+        public CategoriesManager(IUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            IRepository<Category> categoriesRepository = UnitOfWork.GetRepository<Category>();
+
+            IEnumerable<Category> categories = categoriesRepository.GetAll();
+
+            return categories;
+        }
+
+        public void AddCategory(Category category)
+        {
+            IRepository<Category> categoriesRepository = UnitOfWork.GetRepository<Category>();
+
+            categoriesRepository.Add(category);
+
+            UnitOfWork.SaveChanges();
+        }
+
+
+    }
+}
