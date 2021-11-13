c77518e2669a4d37903d037d63f59e7cd0dad510
Juan Camilo Sánchez Taveras <jsanchez@universal.com.do>
2021-11-13 11:24:20 -0400

    Added PeriodManager implementation and tests

---
+++ Core/Managers/Implementations/PeriodsManager.cs
@@ -0,0 +1,37 @@
+﻿using FinanceManagement.Core.Entities;
+using FinanceManagement.Core.Repositories;
+using FinanceManagement.Core.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceManagement.Core.Managers.Implementations
+{
+    public class PeriodsManager : IPeriodsManager
+    {
+        private readonly IUnitOfWork UnitOfWork;
+
+        public PeriodsManager(IUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+        }
+
+        public void AddPeriod(Period period)
+        {
+            IRepository<Period> periodsRepository = UnitOfWork.GetRepository<Period>();
+
+            periodsRepository.Add(period);
+
+            UnitOfWork.SaveChanges();
+        }
+
+        public IEnumerable<Period> GetPeriods()
+        {
+            IRepository<Period> periodsRepository = UnitOfWork.GetRepository<Period>();
+
+            IEnumerable<Period> periods = periodsRepository.GetAll();
+
+            return periods;
+        }
+    }
+}
---
+++ Tests/PeriodsManagerTests.cs
@@ -0,0 +1,84 @@
+﻿using FinanceManagement.Core.Entities;
+using FinanceManagement.Core.Managers;
+using FinanceManagement.Core.Managers.Implementations;
+using FinanceManagement.Core.Repositories;
+using FinanceManagement.Core.UnitOfWork;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using Xunit;
+
+namespace FinanceManagement.Tests
+{
+    public class PeriodsManagerTests
+    {
+
+        [Fact]
+        public void AddPeriod_Adds_Periods_Correctly_To_Repository()
+        {
+            //Setup
+            List<Period> mockPeriodsDatabase = new List<Period>();
+            Mock<IRepository<Period>> mockPeriodsRepository = new Mock<IRepository<Period>>();
+            mockPeriodsRepository.Setup(repository => repository.Add(It.IsAny<Period>())).Callback((Period period) => mockPeriodsDatabase.Add(period));
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(unitOfWork => unitOfWork.GetRepository<Period>()).Returns(mockPeriodsRepository.Object);
+            PeriodsManager periodsManager = new PeriodsManager(mockUnitOfWork.Object);
+
+            //Arrange
+            Period expectedPeriod = new Period
+            {
+                Id = 1,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddDays(13)
+     
+            };
+            string expectedPeriodString = JsonSerializer.Serialize(expectedPeriod);
+
+            //Act
+            periodsManager.AddPeriod(expectedPeriod);
+            Period addedPeriod = mockPeriodsDatabase.Single();
+            string addedPeriodString = JsonSerializer.Serialize(addedPeriod);
+
+            //Assert
+            Assert.Equal(expectedPeriodString, addedPeriodString);
+        }
+
+
+        [Fact]
+        public void GetPeriods_Returns_All_Periods_From_Repository()
+        {
+            //Setup
+            List<Period> mockPeriodsDatabase = new List<Period>();
+            Mock<IRepository<Period>> mockPeriodsRepository = new Mock<IRepository<Period>>();
+            mockPeriodsRepository.Setup(repository => repository.GetAll(null, null, "")).Returns(mockPeriodsDatabase);
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(unitOfWork => unitOfWork.GetRepository<Period>()).Returns(mockPeriodsRepository.Object);
+            PeriodsManager periodsManager = new PeriodsManager(mockUnitOfWork.Object);
+
+
+            //Arrange
+            mockPeriodsDatabase.Add(new Period
+            {
+                Id = 1,
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddDays(13)
+            });
+            mockPeriodsDatabase.Add(new Period
+            {
+                Id = 2,
+                StartDate = DateTime.Now.AddDays(13),
+                EndDate = DateTime.Now.AddDays(26)
+            }
+            );
+
+            //Act
+            IEnumerable<Period> returnedPeriods = periodsManager.GetPeriods();
+
+            //Assert
+            Assert.Equal(mockPeriodsDatabase, returnedPeriods);
+
+        }
+    }
+}
