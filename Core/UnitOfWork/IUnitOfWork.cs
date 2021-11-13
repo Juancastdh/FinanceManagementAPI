54bc5a32ca451749495eb85426aa209cb962950b
Juan Camilo Sánchez Taveras <jsanchez@universal.com.do>
2021-11-12 20:29:06 -0400

    Adding UnitOfWork classes

---
+++ Core/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using FinanceManagement.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceManagement.Core.UnitOfWork
+{
+    public interface IUnitOfWork: IDisposable
+    {
+
+        public IRepository<T> GetRepository<T>() where T : class;
+        void SaveChanges();
+    }
+}
---
+++ Services/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,51 @@
+﻿using FinanceManagement.Core.Repositories;
+using FinanceManagement.Core.UnitOfWork;
+using FinanceManagement.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceManagement.Services.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private DatabaseContext Context;
+
+        private bool disposed = false;
+
+        public UnitOfWork(DatabaseContext context)
+        {
+            Context = context;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    Context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public IRepository<T> GetRepository<T>() where T : class
+        {
+            return new BaseRepositoryService<T>(Context);
+        }
+
+        public void SaveChanges()
+        {
+            Context.SaveChanges();
+        }
+    }
+}
