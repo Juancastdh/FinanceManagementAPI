5a5e13833d0e7c11d1e367e2a9441c2be4fc4488
Juan Camilo Sanchez Taveras <juancastdh@gmail.com>
2025-01-06 19:17:19 -0400

    Added unit test that verifies Account is nullable in financial transaction

--- Tests/FinancialTransactionsManagerTests.cs
+++ Tests/FinancialTransactionsManagerTests.cs
@@ -707,6 +707,45 @@ namespace FinanceManagement.Tests
 
         }
 
+        [Fact]
+        public void AddFinancialTransaction_Adds_FinancialTransaction_With_Empty_Account_Correctly()
+        {
+            //Setup
+            List<FinancialTransaction> mockFinancialTransactionsDatabase = new List<FinancialTransaction>();
+            Mock<IRepository<FinancialTransaction>> mockFinancialTransactionsRepository = new Mock<IRepository<FinancialTransaction>>();
+            mockFinancialTransactionsRepository.Setup(repository => repository.Add(It.IsAny<FinancialTransaction>())).Callback((FinancialTransaction financialTransaction) => mockFinancialTransactionsDatabase.Add(financialTransaction));
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockUnitOfWork.Setup(unitOfWork => unitOfWork.GetRepository<FinancialTransaction>()).Returns(mockFinancialTransactionsRepository.Object);
+            FinancialTransactionsManager financialTransactionsManager = new FinancialTransactionsManager(mockUnitOfWork.Object);
+
+            //Arrange
+            FinancialTransaction expectedFinancialTransaction = new FinancialTransaction
+            {
+                Id = 1,
+                Date = DateTime.Now,
+                Description = "Test Transaction",
+                IsExpense = true,
+                Value = 1000,
+                CategoryId = 1,
+                Category = new Category
+                {
+                    Id = 1,
+                    Name = "TestCategory",
+                    Percentage = 5
+                }
+            };
+
+            string expectedFinancialTransactionString = JsonSerializer.Serialize(expectedFinancialTransaction);
+
+            //Act
+            financialTransactionsManager.AddFinancialTransaction(expectedFinancialTransaction);
+            FinancialTransaction addedFinancialTransaction = mockFinancialTransactionsDatabase.Single();
+            string addedFinancialTransactionString = JsonSerializer.Serialize(addedFinancialTransaction);
+
+            //Assert
+            Assert.Equal(expectedFinancialTransactionString, addedFinancialTransactionString);
+        }
+
 
 
 
