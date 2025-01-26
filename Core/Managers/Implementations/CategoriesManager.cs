7e0bc536bf225b4b5647e45fb8e222070a732b16
Juan Camilo Sanchez Taveras <juancastdh@gmail.com>
2025-01-26 19:50:53 -0400

    Added optional deleted filter to get categories service

--- Core/Managers/Implementations/CategoriesManager.cs
+++ Core/Managers/Implementations/CategoriesManager.cs
@@ -18,13 +18,24 @@ namespace FinanceManagement.Core.Managers.Implementations
             UnitOfWork = unitOfWork;
         }
 
-        public IEnumerable<Category> GetAllCategories()
+        public IEnumerable<Category> GetAllCategories(bool? deleted = null)
         {
 
 
             IRepository<Category> categoriesRepository = UnitOfWork.GetRepository<Category>();
 
-            IEnumerable<Category> categories = categoriesRepository.GetAll();
+            IEnumerable<Category> categories;
+
+            if (deleted != null)
+            {
+                categories = categoriesRepository.GetAll(category => category.Deleted == deleted);
+            }
+            else
+            {
+                categories = categoriesRepository.GetAll();
+            }
+
+
 
             return categories;
 
