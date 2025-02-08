7f11f67d873eaa0ca30ac62f0266647b1dcf46e0
Juan Camilo Sanchez Taveras <juancastdh@gmail.com>
2025-02-08 16:17:01 -0400

    Modified DeletePeriodById method so that it marks period as deleted instead of deleting, when the period is the latest.

--- Core/Managers/Implementations/PeriodsManager.cs
+++ Core/Managers/Implementations/PeriodsManager.cs
@@ -5,6 +5,7 @@ using FinanceManagement.Core.UnitOfWork;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace FinanceManagement.Core.Managers.Implementations
@@ -32,8 +33,16 @@ namespace FinanceManagement.Core.Managers.Implementations
         public void DeletePeriodById(int id)
         {
             IRepository<Period> periodsRepository = UnitOfWork.GetRepository<Period>();
+            Period periodToDelete = periodsRepository.GetById(id);
 
-            periodsRepository.DeleteById(id);
+            if (IsLatestPeriod(periodToDelete) == false)
+            {
+                throw new InvalidOperationException("Specified period is not the latest. Only the latest period can be deleted");
+            }
+
+            periodToDelete.Deleted = true;
+
+            periodsRepository.Update(periodToDelete);
 
             UnitOfWork.SaveChanges();
         }
@@ -74,6 +83,29 @@ namespace FinanceManagement.Core.Managers.Implementations
             periodsRepository.Update(period);
             UnitOfWork.SaveChanges();
 
+        }
+
+        private Period GetLatestPeriod()
+        {
+            IRepository<Period> periodsRepository = UnitOfWork.GetRepository<Period>();
+            IEnumerable<Period> periods = periodsRepository.GetAll(period => period.Deleted == false, periods => periods.OrderByDescending(period => period.StartDate));
+            Period latestPeriod = periods.First();
+
+            return latestPeriod;
+        }
+
+        private bool IsLatestPeriod(Period period)
+        {
+            bool isLatestPeriod = false;
+
+            Period latestPeriod = GetLatestPeriod();
+
+            if (period.Id == latestPeriod.Id)
+            {
+                isLatestPeriod = true;
+            }
+
+            return isLatestPeriod;
         }
     }
 }
