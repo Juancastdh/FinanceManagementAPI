e5f19c50cc7a1d3b11b9db2c5ae5111ab7adc71a
Juan Camilo Sanchez Taveras <juancastdh@gmail.com>
2026-07-20 17:02:30 -0400

    Added investment fund categories business logic

---
+++ Core/Managers/IInvestmentFundCategoriesManager.cs
@@ -0,0 +1,16 @@
+using FinanceManagement.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceManagement.Core.Managers
+{
+    public interface IInvestmentFundCategoriesManager
+    {
+        IEnumerable<InvestmentFundCategory> GetAllInvestmentFundCategories(bool? deleted = null);
+        void AddInvestmentFundCategory(InvestmentFundCategory investmentFundCategory);
+        InvestmentFundCategory GetInvestmentFundCategoryById(int id);
+        void UpdateInvestmentFundCategory(InvestmentFundCategory investmentFundCategory);
+        void DeleteInvestmentFundCategoryById(int id);
+    }
+}
---
+++ Core/Managers/Implementations/InvestmentFundCategoriesManager.cs
@@ -0,0 +1,91 @@
+using FinanceManagement.Core.Entities;
+using FinanceManagement.Core.Exceptions;
+using FinanceManagement.Core.Repositories;
+using FinanceManagement.Core.UnitOfWork;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceManagement.Core.Managers.Implementations
+{
+    public class InvestmentFundCategoriesManager : IInvestmentFundCategoriesManager
+    {
+        private readonly IUnitOfWork UnitOfWork;
+
+        public InvestmentFundCategoriesManager(IUnitOfWork unitOfWork)
+        {
+            UnitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<InvestmentFundCategory> GetAllInvestmentFundCategories(bool? deleted = null)
+        {
+
+
+            IRepository<InvestmentFundCategory> investmentFundCategoriesRepository = UnitOfWork.GetRepository<InvestmentFundCategory>();
+
+            IEnumerable<InvestmentFundCategory> investmentFundCategories;
+
+            if (deleted != null)
+            {
+                investmentFundCategories = investmentFundCategoriesRepository.GetAll(investmentFundCategory => investmentFundCategory.Deleted == deleted, includeProperties: "InvestmentFund");
+            }
+            else
+            {
+                investmentFundCategories = investmentFundCategoriesRepository.GetAll(includeProperties: "InvestmentFund");
+            }
+
+
+
+            return investmentFundCategories;
+
+
+        }
+
+        public void AddInvestmentFundCategory(InvestmentFundCategory investmentFundCategory)
+        {
+            IRepository<InvestmentFundCategory> investmentFundCategoriesRepository = UnitOfWork.GetRepository<InvestmentFundCategory>();
+
+            investmentFundCategoriesRepository.Add(investmentFundCategory);
+
+            UnitOfWork.SaveChanges();
+        }
+
+        public InvestmentFundCategory GetInvestmentFundCategoryById(int id)
+        {
+            InvestmentFundCategory? investmentFundCategory;
+
+            IRepository<InvestmentFundCategory> investmentFundCategoriesRepository = UnitOfWork.GetRepository<InvestmentFundCategory>();
+            investmentFundCategory = investmentFundCategoriesRepository.GetById(id);
+
+            if (investmentFundCategory == null)
+            {
+                throw new DataNotFoundException();
+            }
+
+            return investmentFundCategory;
+        }
+
+        public void UpdateInvestmentFundCategory(InvestmentFundCategory investmentFundCategory)
+        {
+            IRepository<InvestmentFundCategory> investmentFundCategoriesRepository = UnitOfWork.GetRepository<InvestmentFundCategory>();
+            investmentFundCategoriesRepository.Update(investmentFundCategory);
+            UnitOfWork.SaveChanges();
+        }
+
+        public void DeleteInvestmentFundCategoryById(int id)
+        {
+
+            IRepository<InvestmentFundCategory> investmentFundCategoriesRepository = UnitOfWork.GetRepository<InvestmentFundCategory>();
+
+            InvestmentFundCategory investmentFundCategoryToDelete = investmentFundCategoriesRepository.GetById(id);
+
+            investmentFundCategoryToDelete.Deleted = true;
+
+            investmentFundCategoriesRepository.Update(investmentFundCategoryToDelete);
+
+            UnitOfWork.SaveChanges();
+
+        }
+    }
+}
